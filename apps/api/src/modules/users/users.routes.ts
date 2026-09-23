6f1a3a07c9c8e1be4898ebb399007f076b79499e
Minh Q Pham <gpqminh@gmail.com>
2026-09-23 19:03:06 -0400

    test

--- apps/api/src/modules/users/users.routes.ts
+++ apps/api/src/modules/users/users.routes.ts
@@ -4,3 +4,9 @@ import { getCurrentUser } from './users.controller';
 
 export const usersRouter = Router();
 usersRouter.get('/me', requireSession, getCurrentUser);
+
+//POST /api/users
+//GET /api/users
+//GET /api/users/:id
+//PATCH /api/users/:id
+//DELETE /api/users/:id
